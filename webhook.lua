7bf95ab8306c35d92eb59bfe6d949e640d3400de
ImSoBad123 <138328968+ImSoBad123@users.noreply.github.com>
2023-12-08 23:08:34 +0700

    Update webhook.lua

--- webhook.lua
+++ webhook.lua
@@ -1,10 +1,3 @@
-wait(3)
-print("work")
-_G.DoiServer = true
-_G.TimeDoiServer = "1860"
-
-
-
 while wait() do
         local args = {
             [1] = 1
@@ -42,3 +35,30 @@ while wait() do
         
         game:GetService("ReplicatedStorage"):WaitForChild("Packages"):WaitForChild("_Index"):WaitForChild("sleitnick_net@0.1.0"):WaitForChild("net"):WaitForChild("RF/ClaimPlaytimeReward"):InvokeServer(unpack(args))
 end
+repeat wait() until
+game:IsLoaded()
+print("Start Count")
+wait(1860)
+    local Player = game.Players.LocalPlayer    
+    local Http = game:GetService("HttpService")
+    local TPS = game:GetService("TeleportService")
+    local Api = "https://games.roblox.com/v1/games/"
+
+    local _place,_id = game.PlaceId, game.JobId
+    local _servers = Api.._place.."/servers/Public?sortOrder=Desc&limit=100"
+    function ListServers(cursor)
+       local Raw = game:HttpGet(_servers .. ((cursor and "&cursor="..cursor) or ""))
+       return Http:JSONDecode(Raw)
+    end
+
+    local Next; repeat
+       local Servers = ListServers(Next)
+       for i,v in next, Servers.data do
+           if v.playing < v.maxPlayers and v.id ~= _id then
+               local s,r = pcall(TPS.TeleportToPlaceInstance,TPS,_place,v.id,Player)
+               if s then break end
+           end
+       end
+       
+       Next = Servers.nextPageCursor
+    until not Next
